75d762666aaaf1fa8a1e6ba0a5f882376978ca85
Andre Martins <andre.t.martins@gmail.com>
2018-08-15 14:10:28 +0100

    Updated content.

---
+++ pelicanconf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Andr\xe9 F. T. Martins'
+SITENAME = u'Andr\xe9 F. T. Martins'
+SITEURL = ''
+
+PATH = 'content'
+
+STATIC_PATHS = ['images', 'docs']
+
+TIMEZONE = 'Europe/Lisbon'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = None #() #(('Pelican', 'http://getpelican.com/'),
+         #('Python.org', 'http://python.org/'),
+         #('Jinja2', 'http://jinja.pocoo.org/'),
+         #('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = None #(('github', 'http://github.com/andre-martins'),)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+DISPLAY_CATEGORIES_ON_MENU = False
